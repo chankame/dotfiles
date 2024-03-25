230673472d1f6acf1ca50c8ec602726f366e9f4f
chankame <chan@denkiburo.com>
2024-03-25 10:49:33 +0000

    既存の設定ファイルをバックアップするように

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 cd `dirname $0`
 DOTFILES_DIR=`pwd`
+BACKUP_DIR=$DOTFILES_DIR/backup/$(date +%Y%m%d%H%M%S)
+mkdir -p "$BACKUP_DIR"
 
 git submodule update --init
 
@@ -11,6 +13,10 @@ do
 	[ "$f" = '.DS_Store' ] && continue
 	swp=${f##*.}
 	[ "$swp" = 'swp' ] && continue
+
+	if [ -e ~/$f ]; then
+		cp -r ~/$f "$BACKUP_DIR/$f"
+	fi
 
 	ln -fsv $DOTFILES_DIR/$f ~
 done
